727568538185c669ce81d434dbf20b5d169da05f
yulongtian@sohu-inc,com <yulongtian@sohu-inc.com>
2018-09-03 17:07:25 +0800

    add. project

---
+++ Viper/Viper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Viper
+//
+//  Created by tianyulong on 2018/9/3.
+//  Copyright © 2018年 tianyulong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
